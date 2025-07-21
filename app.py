fe2abefdba7c71e34d73f5856a993174e4a2d7c9
lukee-d <lmdtx17@gmail.com>
2025-07-21 15:47:50 -0500

    fix

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY")
 
 # PostgreSQL setup (Render provides DATABASE_URL)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)  # Fix for Render
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
